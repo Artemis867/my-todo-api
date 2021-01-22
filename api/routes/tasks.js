0086baf73a2f347fe5cd5c17aa3e0f66d27258c9
Artemis867 <dev.ronjovi@gmail.com>
2021-01-23 04:09:38 +0800

    update package.json and removing server.js file

--- api/routes/tasks.js
+++ api/routes/tasks.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const { update } = require('../models/tasks');
 const router = express.Router();
 const Task = require('../models/tasks');
+const mongoose = require('mongoose');
 
 router.get('/all', (req, res, next) => {
     Task.find({}).then(data => {
@@ -14,5 +16,44 @@ router.get('/all', (req, res, next) => {
     });
 });
 
+router.post('/', (req, res, next) => {
+    const params = req.body;
+    const task = new Task({
+        _id: new mongoose.Types.ObjectId(),
+        taskName: params.taskName
+    });
+
+    task.save().then( resp => {
+        res.status(201).json({
+            status: 'success',
+            message: 'access post'
+        });
+    })
+});
+
+router.patch('/:taskId',(req, res, next) => {
+    const params = req.body;
+    const updateObj = {
+        taskName: params.taskName
+    };
+
+    const doUpdateTask = Task.updateOne({_id: req.params.taskId},updateObj);
+    doUpdateTask.then(resp => {
+        res.status(200).json({
+            status: 'success',
+            message: 'access patch'
+        });
+    });
+});
+
+router.delete('/:taskId', (req, res, next) => {
+    const doDelete = Task.findByIdAndDelete(req.params.taskId);
+    doDelete.then(resp => {
+        res.status(200).json({
+            status: 'success',
+            message: 'Document deleted'
+        });
+    });
+});
 
 module.exports = router;
--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const taskRoutes = require('./api/routes/tasks');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const port = process.env.port || 3000;
 
 mongoose.connect("mongodb://administrator:"+process.env.MONGO_ATLAS_PW+"@node-rest-shop-shard-00-00.ozelk.mongodb.net:27017,node-rest-shop-shard-00-01.ozelk.mongodb.net:27017,node-rest-shop-shard-00-02.ozelk.mongodb.net:27017/"+process.env.DB_NAME+"?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin&retryWrites=true&w=majority",
 { useUnifiedTopology: true, useNewUrlParser: true });
+
+// body parser config
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.disable('etag');
 
 // CORS SETUP
 app.use((req, res, next) => {
@@ -37,5 +44,6 @@ app.use((error, req, res, next) => {
     }
 });
 
-const port = process.env.port || 3000;
-app.listen(port);
+app.listen(port, () => {
+    console.log('test');
+});
